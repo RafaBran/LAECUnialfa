b6196f6f6753163729a4af8ebf3008d648c27fa1
Rafael Brandão Neves <bnrafael@outlook.com>
2024-06-21 11:16:43 -0300

    Atualização 21-06-24 11:16

--- assets/scripts/transition.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('load', () => {
-    const backgroundContainer = document.querySelector('.box2__main');
-    setTimeout(() => {
-        backgroundContainer.classList.add('zoomed');
-    }, 1000);
-});
